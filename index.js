9dade34b55836f1de9ee2e7cb162da42297a489d
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-11-08 14:18:26 +1100

    css styling added

--- index.js
+++ index.js
@@ -3,24 +3,30 @@
 // 1 liter = 0.264 gallon
 // 1 kilogram = 2.204 pound
 
-const userInput = document.querySelector("#input-field")
-const inputBtn = document.querySelector("#input-btn")
+let userInput = document.querySelector("#input-field")
+let inputBtn = document.querySelector("#input-btn")
 const feetConv = document.querySelector("#feet")
 const gallonConv = document.querySelector("#gallon")
 const poundConv = document.querySelector("#pound")
 
 inputBtn.addEventListener("click", function(){
-     feetConv.innerHTML = `<h2>${userInput.value} meters = ${(userInput.value * 3.281).toFixed(3)} feet | ${userInput.value} feet = ${(userInput.value / 3.281).toFixed(3)} meters </h2>`
-     gallonConv.innerHTML = `<h2>${userInput.value} liters = ${(userInput.value * 0.264).toFixed(3)} gallons | ${userInput.value} gallons = ${(userInput.value / 0.264).toFixed(3)} liters.</h2>`
-     poundConv.innerHTML = `<h2>${userInput.value} kilograms = ${(userInput.value * 2.204).toFixed(3)} pounds | ${userInput.value} pounds = ${(userInput.value / 2.204).toFixed(3)} kilograms.</h2>`
+     feetConv.textContent = `${userInput.value} meters = ${(userInput.value * 3.281).toFixed(3)} feet | ${userInput.value} feet = ${(userInput.value / 3.281).toFixed(3)} meters.`
+     gallonConv.textContent = `${userInput.value} liters = ${(userInput.value * 0.264).toFixed(3)} gallons | ${userInput.value} gallons = ${(userInput.value / 0.264).toFixed(3)} liters.`
+     poundConv.textContent = `${userInput.value} kilograms = ${(userInput.value * 2.204).toFixed(3)} pounds | ${userInput.value} pounds = ${(userInput.value / 2.204).toFixed(3)} kilograms.`
 })
-
-
 
 inputBtn.addEventListener("click", render)
 
 
+// inputBtn.addEventListener("click", function(){
+//      feetConv.innerHTML = `<h2>${userInput.value} meters = ${(userInput.value * 3.281).toFixed(3)} feet | ${userInput.value} feet = ${(userInput.value / 3.281).toFixed(3)} meters </h2>`
+//      gallonConv.innerHTML = `<h2>${userInput.value} liters = ${(userInput.value * 0.264).toFixed(3)} gallons | ${userInput.value} gallons = ${(userInput.value / 0.264).toFixed(3)} liters.</h2>`
+//      poundConv.innerHTML = `<h2>${userInput.value} kilograms = ${(userInput.value * 2.204).toFixed(3)} pounds | ${userInput.value} pounds = ${(userInput.value / 2.204).toFixed(3)} kilograms.</h2>`
+// })
 
+
+
+// inputBtn.addEventListener("click", render)
 
 
 
